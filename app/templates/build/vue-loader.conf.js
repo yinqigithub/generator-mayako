d8c2631cc483c3696c749d0669dfc13ab83fae53
mayako <freedom21126@gmail.com>
2017-08-11 12:22:08 +0800

    Create vue-loader.conf.js
    add vue-loader.conf.js

---
+++ app/templates/build/vue-loader.conf.js
@@ -0,0 +1,23 @@
+var utils = require('./utils')
+var config = require('../config')
+var isProduction = process.env.NODE_ENV === 'production';
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  loaders: utils.cssLoaders({
+    sourceMap: isProduction
+      ? config.build.productionSourceMap
+      : config.dev.cssSourceMap,
+    extractCSS: true,
+    extract: isProduction,
+    css: ExtractTextPlugin.extract({
+      use: 'css-loader',
+      fallback: 'vue-style-loader' // <- 这是vue-loader的依赖，所以如果使用npm3，则不需要显式安装
+    })
+  }),
+  postcss: [
+    require('autoprefixer')({
+      browsers: ['last 2 versions']
+    }),require('postcss-cssnext')()
+  ]
+}
